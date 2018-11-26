e72f61c58e6cee4fd9eb818f2c3bea9a7a62c077
JonyFang <jony.chunfang@gmail.com>
2018-11-26 14:16:30 +0800

    Initial Commit

---
+++ FFPopup/FFPopup.h
@@ -0,0 +1,19 @@
+//
+//  FFPopup.h
+//  FFPopup
+//
+//  Created by JonyFang on 2018/11/26.
+//  Copyright © 2018年 JonyFang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FFPopup.
+FOUNDATION_EXPORT double FFPopupVersionNumber;
+
+//! Project version string for FFPopup.
+FOUNDATION_EXPORT const unsigned char FFPopupVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FFPopup/PublicHeader.h>
+
+
