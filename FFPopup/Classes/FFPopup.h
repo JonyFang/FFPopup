1367049122456a7675d9e35898aa3b59871efe13
JonyFang <jony.chunfang@gmail.com>
2018-11-26 14:17:59 +0800

    👋 Init project~
    Signed-off-by: JonyFang <jony.chunfang@gmail.com>

---
+++ FFPopup/Classes/FFPopup.h
@@ -0,0 +1,17 @@
+//
+//  FFPopup.h
+//  FFPopup
+//
+//  Created by JonyFang on 2018/11/26.
+//  Copyright © 2018年 JonyFang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FFPopup : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FFPopup/Classes/FFPopup.m
@@ -0,0 +1,21 @@
+//
+//  FFPopup.m
+//  FFPopup
+//
+//  Created by JonyFang on 2018/11/26.
+//  Copyright © 2018年 JonyFang. All rights reserved.
+//
+
+#import "FFPopup.h"
+
+@implementation FFPopup
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
