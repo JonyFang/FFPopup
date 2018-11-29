eabacbd9582486db638d7a29a1cc49386878d0e4
JonyFang <jony.chunfang@gmail.com>
2018-11-29 11:55:14 +0800

    Configure rootVC
    Signed-off-by: JonyFang <jony.chunfang@gmail.com>

--- Example/FFPopup_Example/Classes/AppDelegate.m
+++ Example/FFPopup_Example/Classes/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "FFHomeViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,12 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    FFHomeViewController *vc = [FFHomeViewController new];
+    UINavigationController *navVC = [[UINavigationController alloc] initWithRootViewController:vc];
+    [self.window setRootViewController:navVC];
+    [self.window makeKeyAndVisible];
+    
     return YES;
 }
 
