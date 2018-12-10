5c19135072d2da0154d8063102584f9bbbd31757
JonyFang <jony.chunfang@gmail.com>
2018-12-10 17:42:34 +0800

    Update the level of the directory
    Signed-off-by: JonyFang <jony.chunfang@gmail.com>

---
+++ Example/FFPopup_Example/Classes/Views/FFHomeTableViewCell.h
@@ -0,0 +1,19 @@
+//
+//  FFHomeTableViewCell.h
+//  FFPopup_Example
+//
+//  Created by JonyFang on 2018/12/10.
+//  Copyright © 2018年 JonyFang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FFHomeTableViewCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
+
+extern NSString *kFFHomeTableViewCell;
---
+++ Example/FFPopup_Example/Classes/Views/FFHomeTableViewCell.m
@@ -0,0 +1,26 @@
+//
+//  FFHomeTableViewCell.m
+//  FFPopup_Example
+//
+//  Created by JonyFang on 2018/12/10.
+//  Copyright © 2018年 JonyFang. All rights reserved.
+//
+
+#import "FFHomeTableViewCell.h"
+
+@implementation FFHomeTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
+
+NSString *kFFHomeTableViewCell = @"FFHomeTableViewCell";
