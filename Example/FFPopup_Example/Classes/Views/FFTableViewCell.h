4e0d4fea72bbec1aba87f35e26b4b4c97b14492f
JonyFang <jony.chunfang@gmail.com>
2018-12-10 17:49:20 +0800

    Add & configure FFTableViewCell
    Signed-off-by: JonyFang <jony.chunfang@gmail.com>

---
+++ Example/FFPopup_Example/Classes/Views/FFTableViewCell.h
@@ -0,0 +1,17 @@
+//
+//  FFTableViewCell.h
+//  FFPopup_Example
+//
+//  Created by JonyFang on 2018/12/10.
+//  Copyright © 2018年 JonyFang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FFTableViewCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/FFPopup_Example/Classes/Views/FFTableViewCell.m
@@ -0,0 +1,31 @@
+//
+//  FFTableViewCell.m
+//  FFPopup_Example
+//
+//  Created by JonyFang on 2018/12/10.
+//  Copyright © 2018年 JonyFang. All rights reserved.
+//
+
+#import "FFTableViewCell.h"
+
+@implementation FFTableViewCell
+
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        for (UIView *view in self.subviews) {
+            if([view isKindOfClass:[UIScrollView class]]) {
+                ///Remove touch delay for iOS 7
+                ((UIScrollView *)view).delaysContentTouches = NO;
+                break;
+            }
+        }
+        self.selectionStyle = UITableViewCellSelectionStyleNone;
+        self.backgroundView.backgroundColor = [UIColor clearColor];
+        self.contentView.backgroundColor = [UIColor clearColor];
+        self.backgroundColor = [UIColor clearColor];
+    }
+    return self;
+}
+
+@end
