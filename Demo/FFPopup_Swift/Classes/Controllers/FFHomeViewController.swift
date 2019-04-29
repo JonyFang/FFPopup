f4c96360da6bdd7c7a8d879678dfbe1cd27a667a
JonyFang <jony.chunfang@gmail.com>
2019-04-29 10:31:45 +0800

    configure swift demo
    Signed-off-by: JonyFang <jony.chunfang@gmail.com>

--- Demo/FFPopup_Swift/Classes/Controllers/FFHomeViewController.swift
+++ Demo/FFPopup_Swift/Classes/Controllers/FFHomeViewController.swift
@@ -19,7 +19,26 @@ class FFHomeViewController: UIViewController {
     // MARK: Private Methods
     fileprivate func setupViews() {
         view.backgroundColor = .white
+        let tap = UITapGestureRecognizer(target: self, action: #selector(showPopup))
+        tap.numberOfTapsRequired = 1
+        self.view.addGestureRecognizer(tap)
     }
+    
+    @objc fileprivate func showPopup() {
+        let popup = FFPopup(contentView: self.alertView, showType: .bounceIn, dismissType: .shrinkOut, maskType: .dimmed, dismissOnBackgroundTouch: true, dismissOnContentTouch: false)
+        let layout = FFPopupLayoutMake(.horizontalLayout_Center, .center)
+        popup.show(with: layout)
+    }
+    
+    // MARK: Properties
+    fileprivate lazy var alertView: BLCustomContentView = {
+        let w = UIScreen.main.bounds.size.width - 30
+        let h = w * 3 / 4
+        let frame = CGRect(x: 0, y: 0, width: w, height: h)
+        let view = BLCustomContentView(frame: frame)
+        view.backgroundColor = .purple
+        return view
+    }()
 
 }
 
